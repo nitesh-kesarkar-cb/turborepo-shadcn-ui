6333340dd02cd1ef71aa06305aca9ce25556537f
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-11-08 16:33:27 +0530

    tailwindcss added

--- apps/admin-app/src/App.tsx
+++ apps/admin-app/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <p className="read-the-docs">
+    <p className="text-red-500 m-5 text-2xl font-bold">
       Click on the Vite and React logos to learn more
     </p>
   )
