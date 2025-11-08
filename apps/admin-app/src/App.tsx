bbc147dc2bdf53ab665ef5590e2c66c115c3837d
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-11-08 16:47:00 +0530

    button component used

--- apps/admin-app/src/App.tsx
+++ apps/admin-app/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Button } from '@repo/ui/components/ui/button'
 import './App.css'
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
   return (
     <p className="text-red-500 m-5 text-2xl font-bold">
       Click on the Vite and React logos to learn more
+
+      <Button>test</Button>
     </p>
   )
 }
